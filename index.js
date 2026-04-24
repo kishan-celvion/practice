b2d1dd871fbbf5ef2e76206c908018b9b05b6212
Kishan Celvion <kishan.celvion@gmail.com>
2026-04-24 17:40:01 +0530

    JSON APIs

--- index.js
+++ index.js
@@ -31,6 +31,7 @@
 
 const express = require('express')
 const app = express()
+
 let notes = [
   {
     id: "1",
@@ -48,17 +49,88 @@ let notes = [
     important: true
   }
 ]
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+app.use(express.json())
+app.use(requestLogger)
+
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
 })
 
-// app.get('/api/notes', (request, response) => {
-//     response.json(notes)
-// })
+app.get('/api/notes', (request, response) => {
+    response.json(notes)
+})
+
 app.get('/api/notes/:id', (request, response) => {
   const id = request.params.id
   const note = notes.find(note => note.id === id)
+  if(note){
+    response.json(note)
+  }
+  else {
+    response.status(400).end()
+  }
+})
+
+// app.post('/api/notes', (request, response) => {
+//   const note = request.body
+//   console.log(note)
+//   response.json(note)
+// })
+
+// app.post('/api/notes', (request, response) => {
+//   const maxId = notes.length > 0
+//     ? Math.max(...notes.map(n => Number(n.id))) 
+//     : 0
+
+//   const note = request.body
+//   note.id = String(maxId + 1)
+
+//   notes = notes.concat(note)
+
+//   response.json(note)
+// })
+
+const generateId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => Number(n.id)))
+    : 0
+  return String(maxId + 1)
+}
+
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+
+  if (!body.content) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+
+  const note = {
+    id: generateId(),
+    content: body.content,
+    important: body.important || false,
+  }
+
+  notes = notes.concat(note)
+
   response.json(note)
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+  const id = request.params.id
+  notes = notes.filter((note) => note.id !== id)
+
+  response.status(204).end()
 })
 
 const PORT = 3001
