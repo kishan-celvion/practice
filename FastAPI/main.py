009c8fd2b674ebc053d80e2bbfdc512b5e4f247f
Kishan Celvion <kishan.celvion@gmail.com>
2026-05-01 14:43:41 +0530

    FastAPI - Query Parameters

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -14,42 +14,102 @@ async def root():
     return {"message": "Hello, World!"}
 
 # Path parameters
-@app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    return {"item_id": item_id}
+# @app.get("/items/{item_id}")
+# async def read_item(item_id: int):
+#     return {"item_id": item_id}
 
 # Query parameters
-@app.get("/users/me")
-async def read_user_me():
-    return {"user_id": "the current user"}
+# @app.get("/users/me")
+# async def read_user_me():
+#     return {"user_id": "the current user"}
 
 # Path parameters with type conversion
-@app.get("/users/{user_id}")
-async def read_user(user_id: str):
-    return {"user_id": user_id}
+# @app.get("/users/{user_id}")
+# async def read_user(user_id: str):
+#     return {"user_id": user_id}
 
 # Multiple path operations with the same path and same HTTP methods
-@app.get("/users")
-async def read_users():
-    return ["Rick", "Morty"]
-
-@app.get("/users")
-async def read_users2():
-    return ["Bean", "Elfo"]
+# @app.get("/users")
+# async def read_users():
+#     return ["Rick", "Morty"]
+# @app.get("/users")
+# async def read_users2():
+#     return ["Bean", "Elfo"]
 
 # Path parameters with Enum
-@app.get("/models/{model_name}")
-async def get_model(model_name: ModelName):
-    if model_name is ModelName.alexnet:
-        return {"model_name": model_name, "message": "Deep Learning FTW!"}
-    if model_name.value == "lenet":
-        return {"model_name": model_name, "message": "LeCNN all the images"}
-    return {"model_name": model_name, "message": "Have some residuals"}
+# @app.get("/models/{model_name}")
+# async def get_model(model_name: ModelName):
+#     if model_name is ModelName.alexnet:
+#         return {"model_name": model_name, "message": "Deep Learning FTW!"}
+#     if model_name.value == "lenet":
+#         return {"model_name": model_name, "message": "LeCNN all the images"}
+#     return {"model_name": model_name, "message": "Have some residuals"}
 
 # Path parameters with path converter
-@app.get("/files/{file_path:path}")
-async def read_file(file_path: str):
-    return {"file_path": file_path}
+# @app.get("/files/{file_path:path}")
+# async def read_file(file_path: str):
+#     return {"file_path": file_path}
 
 
 
+
+
+# Query parameters with default values
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: str, q: str | None = None):
+    if q:
+        return {"item_id": item_id, "q": q}
+    return {"item_id": item_id}
+
+# Query parameters with default values and type conversion
+@app.get("/items/{item_id}")
+async def read_item(item_id: str, q: str | None = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
+# Multiple path parameters and query parameters
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(user_id: int, item_id: str, q: str | None = None, short: bool = False):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
+# Required query parameters --> here, needy 
+@app.get("/items/{item_id}")
+async def read_user_item(item_id: str, needy: str):
+    item = {"item_id": item_id, "needy": needy}
+    return item
+
+# Required query parameters with type conversion and default values
+@app.get("/items/{item_id}")
+async def read_user_item(item_id: str, needy: str, skip: int = 0, limit: int | None = None):
+    item = {"item_id": item_id, "needy": needy, "skip": skip, "limit": limit}
+    return item
+
+# Enum values as query parameters
+@app.get("/items/{item_id}")
+async def read_user_item(item_id: str, model_name: ModelName):
+    item = {"item_id": item_id, "model_name": model_name}
+    if model_name == ModelName.alexnet:
+        item.update({"message": "Deep Learning FTW!"})
+    elif model_name.value == "lenet":
+        item.update({"message": "LeCNN all the images"})
+    else:
+        item.update({"message": "Have some residuals"})
+    return item 
